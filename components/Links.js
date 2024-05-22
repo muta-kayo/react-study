127b2d2df783881b53e0f99846e083a260db107f
kayo-muta <kmty4_luv@yahoo.co.jp>
2024-05-23 01:17:37 +0900

    ♻️ export default の削除

--- components/Links.js
+++ components/Links.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Links() {
+export function Links() {
     return (
             <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
                 <a
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Image from "next/image";
+
 import { Inter } from "next/font/google";
 import { Footer } from "../components/Footer";
-import Links from "@/components/Links";
-import Headline from "@/components/Headline";
+import { Links } from "../components/Links";
+import { Headline } from "../components/Headline";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
